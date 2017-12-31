04298e4b94326e5441dae10a14c3d1960d64bed4
clementia1 <grininma0@gmail.com>
2017-12-31 10:21:06 +0200

    better routing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import mainJS from "./js/main";
 import filmDetails from "./js/filmDetails";
+import inTheaters from "./js/inTheaters";
 
 export function init() {
     if (window.location.hash === "") {
@@ -8,6 +9,8 @@ export function init() {
 
     if (window.location.hash === "#main") {
         mainJS();
+    } else if (window.location.hash === "#inTheaters") {
+        inTheaters();
     } else {
         filmDetails();
     }
